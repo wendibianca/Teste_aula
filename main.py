7eb7996afe6b46c208f3bc48b920ff34dae8e201
Wendi Bianca Costa <wendi.bianca@gmail.com>
2024-11-29 09:46:12 +0000

    Update main.py
    Alterações realizadas por Wendi Costa

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-print("Olá turma do Python!")
-print("Tudo joia!")
+print("Olá Malta do Python!")
+print("Tudo bem!")
 nome: str = input("Qual é o seu nome? ")
-print(f"Olá, {nome}!")
+print(f"Seja Bem Vindo, {nome}!")
